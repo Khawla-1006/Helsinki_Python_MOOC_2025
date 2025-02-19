2b2da22f498a7262ad827dfe2b7e9a02dd3f4bb6
khawla-1006 <khawla.ben.in@gmail.com>
2025-02-19 13:24:34 +0100

    same_word_twice

--- part04-15_same_word_twice/src/same_word_twice.py
+++ part04-15_same_word_twice/src/same_word_twice.py
@@ -1,1 +1,9 @@
 # Write your solution here
+my_words = []
+
+while True :
+    word = input("Word: ")
+    if word in my_words :
+        break
+    my_words.append(word)
+print("You typed in", len(my_words),"different words")
