6372076c29b93004e887cf6d968a5c1b0d022052
khawla-1006 <khawla.ben.in@gmail.com>
2025-02-19 12:49:22 +0100

    spruce

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,11 @@
 # Write your solution here
+def spruce(x) : 
+    print("a spruce!")
+    i = x 
+    while i > 0 :
+        print(" " * (i - 1) + "*" * (x - i + 1) + "*" * (x - i))
+        i -= 1
+    print(" " * (x - 1) + "*")
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     spruce(5)
