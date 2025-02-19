dd70cec940f7d565f42b653f8ea94a79d5bb8b16
khawla-1006 <khawla.ben.in@gmail.com>
2025-02-19 17:33:58 +0100

    unfinished ex

---
+++ part05-04_sudoku_row/src/sudoku_row.py
@@ -0,0 +1,21 @@
+# Write your solution here
+def row_correct(sudoku : list , row_no : int) :
+    occ = []
+    for num in sudoku[row_no] :
+        if num != 0 :
+            occurrence = sudoku[row_no].count(num)
+            occ.append(occurrence)
+    for o in occ : 
+        if o > 1 :
+            return False
+    return True
+
+#another solution :
+#def row_correct(sudoku: list, row_no: int):
+    # numbers = []
+    # for number in sudoku[row_no]:
+    #     if number > 0 and number in numbers:
+    #         return False
+    #     numbers.append(number)
+ 
+    # return True 
---
+++ part05-20_numbers_spelled_out/src/numbers_spelled_out.py
@@ -0,0 +1,17 @@
+# Write your solution here
+def dict_of_numbers() :
+    dict_num = {}
+    dict_num[1] = "one"
+    dict_num[2] = "two"
+    dict_num[3] = "three"
+    dict_num[4] = "four"
+    dict_num[5] = "five"
+    dict_num[6] = "six"
+    dict_num[7] = "seven"
+    dict_num[8] = "eight"
+    dict_num[9] = "nine"
+    dict_num[10] = "ten"
+    dict_num[11] = "eleven"
+    dict_num[12] = "twelve"
+
+
