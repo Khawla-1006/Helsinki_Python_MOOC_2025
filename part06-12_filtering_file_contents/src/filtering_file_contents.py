d68fb421c4e01e358e5eab8d79d3071c0b8c7fa2
khawla-1006 <khawla.ben.in@gmail.com>
2025-03-03 14:36:31 +0100

    model_solution

--- part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -26,4 +26,25 @@ def filter_solutions():
                 line += f"{val};"
             line = line[:-1]
             incorrect_answers.write(line+"\n")
-        
+
+########### MODEL SOLUTION ############
+
+def filter_sol():
+    #open all files 
+    with open("solutions.csv") as source, open("corrects.csv", "w")as correct_file, open("incorrects", "w") as incorrect_file :
+        for row in source :
+            pieces = row.split(";")
+            calculations = pieces[1]
+            results = pieces[2]
+        #addition or substraction ?
+        if "+" in calculations :
+            operands = calculations.split("+")
+            correct = int(operands[0]) + int(operands[1]) == int(results) #correct is true or false based on whether the calculations were right or wrong!
+        else :
+            operands = calculations.split("-")
+            correct = int(operands[0]) - int(operands[1]) == int(results)
+        #write to files :
+        if correct :
+            correct_file.write(row)
+        else :
+            incorrect_file.write(row)
